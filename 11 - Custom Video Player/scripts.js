06875ed95fe4d352cb1cb3bb1c77a1b73b2dec2a
matchashiba <recr38@gmail.com>
2020-05-28 00:20:17 +0000

    Grabbed player elements from DOM Need to figure out volume and playback

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,12 @@
+// Grab all player elements
+const player = document.querySelector('.player');
+const display = player.querySelector('.viewer');
+const controls = player.querySelector('.player__controls');
+const progress = controls.querySelector('.progress');
+const toggle = controls.querySelector('.toggle');
+// const volume = controls.querySelector('[name="volume]');
+// const playback = controls.querySelector('input [name="playbackRate"]');
+// Create handlers
+
+
+// Hook up handler functions with listeners
