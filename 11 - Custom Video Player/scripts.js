a5b4bf86eb5477744577a057a8713dc01bd024a6
matchashiba <recr38@gmail.com>
2020-05-29 00:03:18 +0000

    Selected all player elements Assigned player elements to variables Added event handler for display

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,11 +2,13 @@
 const player = document.querySelector('.player');
 const display = player.querySelector('.viewer');
 const controls = player.querySelector('.player__controls');
-const progress = controls.querySelector('.progress');
-const toggle = controls.querySelector('.toggle');
-// const volume = controls.querySelector('[name="volume]');
-// const playback = controls.querySelector('input [name="playbackRate"]');
+const progressBar = player.querySelector('.progress');
+const progress = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip');
+const ranges = player.querySelectorAll('.player__slider');
+
 // Create handlers
 
-
 // Hook up handler functions with listeners
+display.addEventListener('click', handleVideo);
